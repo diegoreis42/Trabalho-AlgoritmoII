dc7ff9db4f31d2598d5153dc0ecc1cd617178fe9
FFernandes4280 <d2022002088@unifei.edu.br>
2023-04-22 17:06:07 -0300

    funções trocadas de lugar

--- quick/repetidor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..30}
-do
-  gcc -o programa quick_sort.c  # compila o código em C, gerando um executável diferente a cada iteração
-  ./programa  # executa o programa recém-compilado
-  time ./programa
-done
