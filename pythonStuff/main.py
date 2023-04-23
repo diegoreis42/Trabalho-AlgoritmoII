64837a52827ede056f4031add0d56498586adead
merlin <reisdiego144@gmail.com>
2023-04-23 04:11:42 -0300

    automatizando a fita

--- pythonStuff/main.py
+++ pythonStuff/main.py
@@ -13,14 +13,16 @@ parser = ap.ArgumentParser(
 
 parser.add_argument("-f", "--file", dest="fileName", help="Nome do arquivo a ser criado")
 parser.add_argument("-n", "--numbers", dest="nrandNum", type=int, help="Quantidade de numeros aleatorios a ser criado, 'n' tambem define o range dos numeros")
-
-
+parser.add_argument("-O", "--ordered", action="store_true", help="Indica se a sequencia de numeros deve ser ordenada")
+parser.add_argument("-A", "--automatic", action="store_true", help="Automatiza a criaçao de arquivos aleatorios, de tamanhos 100, 1000, 10000, 100000, 1000000")
 
 if __name__ == "__main__":
     
     args = parser.parse_args()
-    
-    rfg.writeRandFile(args.fileName, args.nrandNum)
+    if args.automatic:
+        rfg.autoGen(args.ordered)
+    else:
+        rfg.writeRandFile(args.fileName, args.nrandNum, args.ordered)
     
 
 
--- pythonStuff/rand_files_gen.py
+++ pythonStuff/rand_files_gen.py
@@ -4,12 +4,29 @@ import random as r
 
 
 
-def writeRandFile(nome, n: int):
-    path = "../rand_files/"
-    nome = path + nome
+def writeRandFile(nome, n: int, ordered = False):
+    list = []
+
+    for i in range(n):
+       list.append(r.randrange(n))
+
+    if(ordered):
+        list.sort()
+        path = "../rand_files/ordered/"
+        nome = path + nome
+    else:
+        path = "../rand_files/"
+        nome = path + nome
+
     with open(nome, 'x') as file:
         for x in range(n):
-            file.write("%d\n" % r.randrange(n))
+            file.write("%d\n" % list[x])
         file.close()
 
 
+def autoGen(ordered = False):
+    fileNames = ["100.txt", "1_000.txt", "10_000.txt", "100_000.txt", "1_000_000.txt"]
+
+    for f in fileNames:
+        print(f)
+        writeRandFile(f, 10 ** (fileNames.index(f) + 2), ordered)
